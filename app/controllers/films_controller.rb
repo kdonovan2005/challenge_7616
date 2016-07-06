29d6b382b0fc49f62df330daf35c0213fc0ac10a
kdonovan2005 <kdonovan2005@gmail.com>
2016-07-06 16:35:03 -0400

    returning title, budget, year

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,11 +2,7 @@ class FilmsController < ApplicationController
 
   def index
     @films = Film.get_data
-    @budget = Film.get_budget
-  end
-
-  def show
-    @film = Film.find(params[:id])
+    @film = Film.create
   end
 
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,31 +1,57 @@
 class Film < ActiveRecord::Base
 
   def self.get_data
+    #sets home url
     url = 'http://oscars.yipitdata.com/'
+    #passes url in RestClient gem
     response = RestClient.get(url)
+    #uses JSON parser to parse RestClient response
     parsed = JSON.parse(response)
     results = parsed["results"]
+    #returns parsed results
     results
   end
 
-  def self.get_budget
-    url = self.get_details_url
-    response = RestClient.get(url)
-    parsed = JSON.parse(response)
-    results = parsed["Budget"]
-    results
-    binding.pry
-  end
-
-  def self.get_details_url
-    results = self.get_data
-    detail_url = ""
-    results.each do |result|
-      result["films"].each do |film|
-        detail_url = film["Detail URL"]
+  def get_budget(url)
+    begin
+      #passes detail URL into RestClient gem
+      response = RestClient.get(url)
+      #uses JSON parser to parse RestClient response
+      parsed = JSON.parse(response)
+      @budget = parsed["Budget"]
+      #checks to see if budget is nil; returns budget converted to integer if not nil
+      if @budget == nil
+        @budget = "Data Unavailable"
+      else
+        convert_budget_to_integer
       end
+      @budget
+      #rescues from bad detail URLs
+    rescue => e
+      puts "Bad URL"
     end
-    detail_url
+  end
+
+  def convert_budget_to_integer
+    #replaces [ i ] from end of budget string response with ""
+    @budget.sub /\s*\[.+\]$/, ""
+    #checks if the budget string includes the word "million"
+    if @budget.include? "million"
+      convert_from_million
+    else
+      convert_from_integer_string
+    end
+  end
+
+  def convert_from_million
+    #separates the $ from the number and word, then splits again to separate the number from 'million' then converts to float
+    i = @budget.split("$").last.split(" ")[0].to_f
+    #multiples the number by 1 million to return the integer
+    i * 1000000
+  end
+
+  def convert_from_integer_string
+    @budget.split("$").last.to_f
   end
 
 end
